3c9a4b7b5b2f4cb19622446c6af32cd9f6f9dedc
primprsr <60314167+primprsr@users.noreply.github.com>
2025-04-18 01:16:27 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+// ปุ่มเปิด/ปิดเพลง
+const music = document.getElementById("bgMusic");
+const toggleBtn = document.getElementById("toggleMusic");
+
+let isPlaying = false;
+
+toggleBtn.addEventListener("click", () => {
+  if (isPlaying) {
+    music.pause();
+    isPlaying = false;
+  } else {
+    music.play();
+    isPlaying = true;
+  }
+});
 
