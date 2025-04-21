6f94dce635317d403834d4fe4f5a66e28b812b52
primprsr <60314167+primprsr@users.noreply.github.com>
2025-04-22 02:52:16 +0700

    Create tarotData.js

---
+++ js/tarotData.js
@@ -0,0 +1,12 @@
+const tarotCards = [
+  {
+    name: "The Fool",
+    image: "assets/tarot/major/00-the-fool.png",
+    message: "A new journey begins. Trust and leap forward with a light heart."
+  },
+  {
+    name: "The Magician",
+    image: "assets/tarot/major/01-the-magician.png",
+    message: "You already have everything you need inside you. Use your power wisely."
+  }
+];
